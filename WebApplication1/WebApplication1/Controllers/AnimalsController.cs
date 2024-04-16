f11d4744a0b18b41a3d1c5a8f9725755d5cc1568
Błażej Kujawiński <sxxxx@pjwstk.edu.pl>
2024-04-16 11:31:53 +0200

    add dataBase handle

---
+++ WebApplication1/WebApplication1/Controllers/AnimalsController.cs
@@ -0,0 +1,47 @@
+﻿using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration iConfiguration)
+    {
+        _configuration = iConfiguration;
+    }
+
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
+        SqlCommand command = new SqlCommand();
+        command.Connection = connection;
+        command.CommandText = "SELECT * from Animal";
+
+        var reader = command.ExecuteReader();
+
+        List<Animal> animals = new List<Animal>();
+        int idAnimalOrdinal = reader.GetOrdinal("idAnimal");
+        int nameOrdinal = reader.GetOrdinal("name");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                idAnimal = reader.GetInt32(idAnimalOrdinal),
+                name = reader.GetString(nameOrdinal)
+                
+            });
+        }
+        
+        return Ok(animals);
+    }
+
+
+
+}
---
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models;
+
+public class Animal
+{
+    public int idAnimal { get; set; }
+    public string name { get; set; }
+    
+}
