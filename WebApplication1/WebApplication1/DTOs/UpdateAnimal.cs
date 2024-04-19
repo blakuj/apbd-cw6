5fe5075a0f9f452b58776db48c773ad4e3282b79
Blazej_Kujawinski <blakuj@gmail.com>
2024-04-19 20:17:47 +0200

    add UpdateAnimal DTOs

---
+++ WebApplication1/WebApplication1/DTOs/UpdateAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs;
+
+public class UpdateAnimal
+{
+    [MaxLength(200)]
+    public string name { get; set; }
+    
+    [MaxLength(200)]
+    public string? description { get; set; }
+    
+    [MaxLength(200)]
+    public string category { get; set; }
+    
+    [MaxLength(200)]
+    public string area { get; set; }
+}
