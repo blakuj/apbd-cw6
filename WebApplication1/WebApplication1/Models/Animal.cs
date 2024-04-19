e14963e84e3e530bd9fd8b411b3a382bc7f3d0ae
Blazej_Kujawinski <blakuj@gmail.com>
2024-04-19 20:15:30 +0200

    add missing columns

--- WebApplication1/WebApplication1/Models/Animal.cs
+++ WebApplication1/WebApplication1/Models/Animal.cs
@@ -4,5 +4,10 @@ public class Animal
 {
     public int idAnimal { get; set; }
     public string name { get; set; }
+
+    public string description { get; set; }
     
+    public string category { get; set; }
+    public string area { get; set; }
+
 }
